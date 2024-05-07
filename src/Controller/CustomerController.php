eaa058f4bab2fcd2e5972131deb0b98b8398fed4
JraDipa <230010037@stikom-bali.ac.id>
2024-05-07 16:53:43 +0800

    Adding Customer Controller

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,37 @@
+
+<?php
+require_once __DIR__ . '/../Model/Customers.php';
+
+class CustomerController {
+    private $customerModel;
+
+    public function __construct() {
+        $this->customerModel = new Customers();
+    }
+
+    public function getAllCustomers() {
+        $customers = $this->customerModel->getAllCustomers();
+        return $customers;
+    }
+
+    public function getCustomerById($customerId) {
+        $customer = $this->customerModel->getCustomerById($customerId);
+        return $customer;
+    }
+
+    public function addCustomer($data) {
+        $this->customerModel->addCustomer($data);
+        return "Customer berhasil ditambahkan";
+    }
+
+    public function updateCustomer($customerId, $data) {
+        $this->customerModel->updateCustomer($customerId, $data);
+        return "Customer berhasil diperbarui";
+    }
+
+    public function deleteCustomer($customerId) {
+        $this->customerModel->deleteCustomer($customerId);
+        return "Customer berhasil dihapus";
+    }
+}
+?>
